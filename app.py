1ad4bf0bd0fba24d5f5a634312831acbc47d3340
sunshine818 <49012524+sunshine818@users.noreply.github.com>
2020-11-29 19:36:55 -0600

    me

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from src.database import add_reminder, list_reminders
-
+from src.deadlined_reminders import DateReminder
+from src.external_reminders import EveningReminder
 
 def handle_input():
     choice = input("Choice: ")
@@ -14,7 +15,7 @@ def handle_input():
         reminder = input("What would you like to be reminded about?: ")
         date = input("When is that due?: ")
 
-        add_reminder(reminder, date)
+        add_reminder(reminder, date, EveningReminder)
         list_reminders()
     else:
         print("Invalid menu option")
