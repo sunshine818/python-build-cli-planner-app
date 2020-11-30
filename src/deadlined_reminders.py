8c3bdf5b390df43be9c9fd21201ae3e76e2fa8f0
sunshine818 <49012524+sunshine818@users.noreply.github.com>
2020-11-29 19:12:45 -0600

    stuff

---
+++ src/deadlined_reminders.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 29 18:58:06 2020
+
+@author: luca
+"""
+
+from abc import ABCMeta, abstractmethod
+from collections.abc import Iterable
+
+class DeadlinedMetaReminder(Iterable):
+    pass
--- src/reminder.py
+++ src/reminder.py
@@ -5,3 +5,8 @@ class PrefixedReminder:
     def __init__(self, prefix="Hey, don't forget to "):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
+        
+class PoliteReminder(PrefixedReminder):
+    def __init__(self, text):
+        super().__init__("Fix this, please")
+        self.text = self.prefix + text
